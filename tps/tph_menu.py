8d1af2d1fbaad2d30fcfb1130ee3652763b71cee
hark130 <hark130@yahoo.com>
2021-11-06 22:47:52 -0500

    menu: Polished up menu module
    - Added module docstring
    - Added get_choice() docstring
    - Extricated get_choice() input validation into separate function
    - Cleaned up imports
    - Refactored get_choice() so that choice_type *must* be a data type (Any is no longer accepted)
    
    Changes to be committed:
    	modified:   tps/tph_menu.py

--- tps/tph_menu.py
+++ tps/tph_menu.py
@@ -1,8 +1,20 @@
+"""Defines Two Point Science package modular menu functionality.
+
+Defines a get_choice() API and Menu namedtuple to reduce redundancy.
+
+    Typical usage example:
+
+    from tps.tph_menu import get_choice, Menu
+    choice_menu = Menu('CHOOSE!', {1: 'like', 2: 'love', 3: 'LOVE'})
+    feeling = get_choice(choice_menu, choice_type=int)
+    print(f'You {feeling} this science!')
+"""
+
 # Standard Imports
 from collections import namedtuple
 
 # Third Party Imports
-from typing import Any, _SpecialForm
+from typing import Any
 
 # Local Imports
 from tps.tph_misc import clear_screen as clr_screen
@@ -10,35 +22,36 @@ from tps.tph_misc import clear_screen as clr_screen
 Menu = namedtuple('Menu', 'name dictionary')
 
 
-def get_choice(tph_menu: Menu, clear_screen: bool = True, choice_type: type = Any,
+def get_choice(tph_menu: Menu, clear_screen: bool = True, choice_type: type = str,
                max_chances: int = 3) -> Any:
+    """Prints a menu, reads/converts user input, returns choice.
+
+    Prints the name of the tph_menu and then a list of selections.  Reads user input, converts
+    the input to choice_type (default: str).  Reads the choice from the tph_menu and returns it.
+
+    Args:
+        tph_menu: Named tuple, defined in the module, containing the menu name and dictionary
+            of user choices.
+        clear_screen: Optional; Clears the screen, in operating system-appropriate ways, before
+            printing anything from tph_menu.
+        choice_type: Optional; The data type to convert the user's selection into.  PRO TIP: This
+            should be the same data type as the keys in your tph_menu.dictionary.
+        max_chances: Optional; Maximum number of inputs to accept from the user before it gives up.
+
+    Returns:
+        A value from tph_menu.dictionary that matches user input.
+
+    Raises:
+        TypeError: error is not an Exception (or inherited from) type.
+    """
     # LOCAL VARIABLES
     user_choice = None  # User input
     num_attempts = 0    # Number of attempts at taking user input
     dict_value = None   # Value associated with the user choice
 
     # INPUT VALIDATION
-    # tph_menu
-    if not isinstance(tph_menu, Menu):
-        raise TypeError(f'The tph_menu must of type Menu instead of {type(tph_menu)}')
-    if not isinstance(tph_menu.name, str):
-        raise TypeError(f'The tph_menu must of type str instead of {type(tph_menu.name)}')
-    if not isinstance(tph_menu.dictionary, dict):
-        raise TypeError(f'The tph_menu must of type dict instead of {type(tph_menu.dictionary)}')
-    if not tph_menu.dictionary:
-        raise ValueError('The tph_menu can not be empty')
-    # clear_screen
-    if not isinstance(clear_screen, bool):
-        raise TypeError('The clear_screen argument must of type bool instead of '
-                        f'{type(clear_screen)}')
-    # choice_type
-    if not isinstance(choice_type, type) and not isinstance(choice_type, _SpecialForm):
-        raise TypeError(f'The choice_type argument can not be of type {type(choice_type)}')
-    # max_chances
-    if not isinstance(max_chances, int):
-        raise TypeError(f'The max_chances argument can not be of type {type(max_chances)}')
-    if max_chances < 1:
-        raise ValueError(f'The max_chances argument must be greater than zero')
+    _validate_get_choice(tph_menu=tph_menu, clear_screen=clear_screen, choice_type=choice_type,
+                         max_chances=max_chances)
 
     # DO IT
     # Clear Screen
@@ -48,9 +61,9 @@ def get_choice(tph_menu: Menu, clear_screen: bool = True, choice_type: type = An
     print(tph_menu.name)
     for option, description in tph_menu.dictionary.items():
         print(f'{option}: {description}')
-    while (True):
+    while True:
         # Take User Input
-        user_choice = read_user_input(choice_type = int)
+        user_choice = read_user_input(choice_type=choice_type)
         num_attempts += 1
         # Validate User Input
         try:
@@ -58,8 +71,7 @@ def get_choice(tph_menu: Menu, clear_screen: bool = True, choice_type: type = An
         except KeyError:
             if num_attempts >= max_chances:
                 raise RuntimeError('User failures exceeded maximum chances')
-            else:
-                print(f'{user_choice} is an invalid selection.  Try again.')
+            print(f'{user_choice} is an invalid selection.  Try again.')
         else:
             return dict_value
 
@@ -86,9 +98,7 @@ def read_user_input(choice_type: type) -> Any:
 
     # INPUT VALIDATION
     # choice_type
-    if isinstance(choice_type, _SpecialForm):
-        choice_type = str
-    elif not isinstance(choice_type, type):
+    if not isinstance(choice_type, type):
         raise TypeError(f'The choice_type argument can not be of type {type(choice_type)}')
 
     # GET IT
@@ -103,3 +113,29 @@ def read_user_input(choice_type: type) -> Any:
 
     # DONE
     return user_choice
+
+
+def _validate_get_choice(tph_menu: Menu, clear_screen: bool, choice_type: type, max_chances: int):
+    """Validate input on behalf of get_choice()."""
+    # INPUT VALIDATION
+    # tph_menu
+    if not isinstance(tph_menu, Menu):
+        raise TypeError(f'The tph_menu must of type Menu instead of {type(tph_menu)}')
+    if not isinstance(tph_menu.name, str):
+        raise TypeError(f'The tph_menu must of type str instead of {type(tph_menu.name)}')
+    if not isinstance(tph_menu.dictionary, dict):
+        raise TypeError(f'The tph_menu must of type dict instead of {type(tph_menu.dictionary)}')
+    if not tph_menu.dictionary:
+        raise ValueError('The tph_menu can not be empty')
+    # clear_screen
+    if not isinstance(clear_screen, bool):
+        raise TypeError('The clear_screen argument must of type bool instead of '
+                        f'{type(clear_screen)}')
+    # choice_type
+    if not isinstance(choice_type, type):
+        raise TypeError(f'The choice_type argument can not be of type {type(choice_type)}')
+    # max_chances
+    if not isinstance(max_chances, int):
+        raise TypeError(f'The max_chances argument can not be of type {type(max_chances)}')
+    if max_chances < 1:
+        raise ValueError(f'The max_chances value ({max_chances}) must be greater than zero')
