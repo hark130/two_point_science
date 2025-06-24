f9e20f7468ff9699d47bc0cd12b497f3b72f3d23
hark130 <hark130@yahoo.com>
2025-06-24 16:49:13 -0500

    graphviz_fix: Removed local dgraph shunt
    No need to make *two* graphs...
    
    Changes to be committed:
    	modified:   tps/dgraph.py

--- tps/dgraph.py
+++ tps/dgraph.py
@@ -137,9 +137,6 @@ def create_graph(hospital: TPHHospital, graph_dir: str, sep_rooms: bool = False,
     graph_obj = add_edges(hospital=hospital, graph=graph_obj, sep_rooms=sep_rooms,
                           focus_node=focus_node)
 
-    # MAKE IT
-    graph_obj.view()
-
     # DONE
     return graph_obj
 # pylint: enable=too-many-arguments
